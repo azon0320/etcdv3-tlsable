18b88dfdd67a6099cdf55f308f9e650ae8119c7a
dormao <452635801@qq.com>
2020-09-01 17:40:10 +0800

    add log

--- etcd.go
+++ etcd.go
@@ -1,6 +1,7 @@
 package etcdv3tls
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/micro/go-micro/v2/config/cmd"
@@ -9,6 +10,7 @@ import (
 )
 
 const (
+	ENV_DEBUG    = "MICRO_ETCDV3_DEBUG"
 	ENV_USERNAME = "MICRO_ETCDV3_USERNAME"
 	ENV_PASSWORD = "MICRO_ETCDV3_PASSWORD"
 	ENV_SECURE   = "MICRO_ETCDV3_SECURE"
@@ -18,15 +20,25 @@ func init() {
 	cmd.DefaultRegistries["etcdv3_tlsable"] = NewRegistry
 }
 
+func isDebug() bool {
+	return os.Getenv(ENV_DEBUG) == "true"
+}
+
 func NewRegistry(opts ...registry.Option) registry.Registry {
 	presetOpts := make([]registry.Option, 0)
 	if os.Getenv(ENV_USERNAME) != "" {
+		if isDebug() {
+			fmt.Println("[ETCDV3TLS] inject credentials")
+		}
 		presetOpts = append(
 			presetOpts,
 			etcd.Auth(os.Getenv(ENV_USERNAME), os.Getenv(ENV_PASSWORD)),
 		)
 	}
 	if os.Getenv(ENV_SECURE) == "true" {
+		if isDebug() {
+			fmt.Println("[ETCDV3TLS] enable secure")
+		}
 		presetOpts = append(
 			presetOpts,
 			registry.Secure(true),
